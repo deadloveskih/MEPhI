e4b12a40ab5140dc2e3f40540d7d81d57fed5971
deadloveskih <stepandedlovskih@gmail.com>
2023-12-13 00:59:57 +0500

    Create main activity
    Create bottom navigation tab.
    Create top navigation tab.
    The logic of transitions by intent.
    Independence from the dark theme.

--- app/src/main/java/com/example/mephi/Splash.java
+++ app/src/main/java/com/example/mephi/Splash.java
@@ -1,14 +1,41 @@
 package com.example.mephi;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Splash extends AppCompatActivity {
+
+    Animation topAnim, bottomAnim;
+    ImageView imgLogo, txtLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.splash);
+
+        imgLogo = findViewById(R.id.image_logo);
+        txtLogo = findViewById(R.id.text_logo);
+
+        imgLogo.setAnimation(topAnim);
+        txtLogo.setAnimation(topAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Welcome.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
--- app/src/main/java/com/example/mephi/Welcome.java
+++ app/src/main/java/com/example/mephi/Welcome.java
@@ -2,13 +2,29 @@ package com.example.mephi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Welcome extends AppCompatActivity {
+
+    Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        signup = findViewById(R.id.signup_button);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Welcome.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
